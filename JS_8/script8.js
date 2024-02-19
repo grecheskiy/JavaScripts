1d70685c39e5fa3fdc8b6512fe8349751c3e6fb9
maxim.grekov <maxim.grekov@v-a-c.org>
2024-02-19 21:50:44 +0300

    add 8

---
+++ JS_8/script8.js
@@ -0,0 +1,26 @@
+console.log("task1");
+const arr1 = [1, 2, 3];
+console.log(arr1[0]);
+console.log(arr1[1]);
+console.log(arr1[2]);
+
+console.log("task2");
+const arr2 = [1, 2, 3, 4, 5, 6, 7];
+console.log(arr2.length);
+
+console.log("task3");
+const arr3 = ['a', 'b', 'c'];
+arr3[0] = 1;
+arr3[1] = -2;
+arr3[2] = -3;
+console.log(arr3[0]);
+console.log(arr3[1]);
+console.log(arr3[2]);
+
+console.log("task4");
+const arr4 = [1, 2, 3];
+for (let i = 0; i < arr4.length; i++) {
+    arr4[i]++;    
+}
+console.log(arr4);
+
